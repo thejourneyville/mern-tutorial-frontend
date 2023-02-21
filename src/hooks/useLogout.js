5d0e5356a51bdfeee51453b2f72babd74b7ad3cc
Benjamin Malberg <bmalberg@protonmail.com>
2023-02-21 13:16:43 -0800

    resets workout context to null on logout

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,14 +1,17 @@
 import { useAuthContext } from "./useAuthContext";
+import { useWorkoutsContext } from "./useWorkoutsContext";
 
 export const useLogout = () => {
     // we get the dispatch function from our context
     const { dispatch } = useAuthContext();
+    const { dispatch: workoutsDispatch } = useWorkoutsContext();
     const logout = () => {
         // remove localstorage
         localStorage.removeItem("user");
 
         // update the auth context
         dispatch({ type: "LOGOUT" });
+        workoutsDispatch({ type: "SET_WORKOUTS", payload: null });
     };
 
     return { logout };
