22bc50be78cc406bc0b084681071ce374ffd0798
Benjamin Malberg <bmalberg@protonmail.com>
2023-02-21 12:59:20 -0800

    added navigate routes to app.js to project workout data from un-logged in users and visa-vera with login/signup pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 // pages and components
 import Home from "./pages/Home";
@@ -7,20 +8,31 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 function App() {
+    const { user } = useAuthContext();
+
     return (
         <div className="App">
             {/* BrowserRouter sits outside of all Routes */}
             <BrowserRouter>
-              <Navbar />
-              <div className="pages">
-                  {/* Routes tag holds each route */}
-                  <Routes>
-                      {/* This route shows the Home component at the "/" path */}
-                      <Route path="/" element={<Home />} />
-                      <Route path="/login" element={<Login />} />
-                      <Route path="/signup" element={<Signup />} />
-                  </Routes>
-              </div>
+                <Navbar />
+                <div className="pages">
+                    {/* Routes tag holds each route */}
+                    <Routes>
+                        {/* This route shows the Home component at the "/" path */}
+                        <Route
+                            path="/"
+                            element={user ? <Home /> : <Navigate to="/login" />}
+                        />
+                        <Route
+                            path="/login"
+                            element={!user ? <Login /> : <Navigate to="/" />}
+                        />
+                        <Route
+                            path="/signup"
+                            element={!user ? <Signup /> : <Navigate to="/" />}
+                        />
+                    </Routes>
+                </div>
             </BrowserRouter>
         </div>
     );
